ac56a4954a19a661b1a477012ca118fbbdcb77f1
valdes-Dekstop <valdes.voufo8@gmail.com>
2022-06-28 21:39:02 +0200

    implement the navbar and footer

--- Projet2vs/src/app/app.module.ts
+++ Projet2vs/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    NavbarComponent,
+
+
   ],
   imports: [
     BrowserModule
