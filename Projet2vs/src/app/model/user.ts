3bf80382b059ad97cf01e1a5d01f996a4770631b
Valdes Voufo <valdes.voufo@rwth-aachen.com>
2022-08-01 16:07:44 +0200

    create some Models

---
+++ Projet2vs/src/app/model/user.ts
@@ -0,0 +1,19 @@
+
+import { entreprise } from "./entreprise";
+import { gender } from "./gender";
+
+export interface user{
+    id:string ;
+    name:string ;
+    vorname:string ;
+    Speudo:string ;
+    gender:gender ;
+    iconUrl:string ;
+    job:string ;
+    jobDomain:string ;
+
+    city:string ;
+    telNumber:string ;
+    email:string ;
+    entreprise:entreprise ;
+}
